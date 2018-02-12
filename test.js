033bb67585355a3f0201a5fabcc70af142fd545b
TaewonyNet <35952395+TaewonyNet@users.noreply.github.com>
2018-02-12 16:19:52 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,22 @@
-try{
-	// 오픈로드 페이지 확인
-	var ifopenload = $('iframe[src*=openload]');
-	if ((ifopenload != null)
-	    && (ifopenload.length > 0))
-	{
-		location.href = $('iframe[src*=openload]')[0].src;
+if (document.getElementsByTagName('iframe').length > 0)
+{
+	try{
+		var iframes = ['gdriveplayer.us', 'openload.co'];
+		for (var i=0; i<iframes.length; i++) {
+			var ifopenload = $('iframe[src*="'+iframes[i]+'"]');
+			if ((ifopenload != null)
+				&& (ifopenload.length > 0))
+			{
+				location.href = ifopenload[0].src;
+			}
+		}
+	}
+	catch(exception) {
+	}
+	finally {
 	}
 }
-catch(exception) {
-}
-finally {
-}
-if (location.host.indexOf("podbbang.com") > -1) { // 팟빵
+if (location.host.indexOf('podbbang.com') > -1) { // 팟빵
 	var cn=$('[name=podbbang_login]')[0];
 	var no=Number(cn.getAttribute('width'));
 	var dn=$('p.download > a')[no];
@@ -21,20 +26,23 @@ if (location.host.indexOf("podbbang.com") > -1) { // 팟빵
 	cn.setAttribute('width', no+1);
 	dn.click();
 }
-else if (location.host == "moviewang.net") { // 무비왕
+else if (location.host == 'moviewang.net') { // 무비왕
     var o = $('div[id=movie_bt] a.OpenLoad');
     if (o.length > 0) {
         o[0].target = '_self';
         o[0].click();
     }
 }
-else if (location.host.indexOf("openload") > -1) { // 오픈로드
+else if (location.host.indexOf('openload') > -1) { // 오픈로드
     location.href = 'http://' + location.host + '/stream/' + $('[id*=stream]')[0].innerText;
 }
-else if (location.host.indexOf("tvnamu") > -1) { // 티비나무
+else if (location.host.indexOf('gdriveplayer.us') > -1) { 
+	location.href = $('video')[0].src;
+}
+else if (location.host.indexOf('tvnamu') > -1) { // 티비나무
     location.href = $('#link_gogovid')[0].value;
 }
-else if (location.host.indexOf("gogovid") > -1) { // 고고vid
+else if (location.host.indexOf('gogovid') > -1) { // 고고vid
     var o = $('<a/>');
     var c = flowplayer('#player').conf;
     o[0].href = c.clip.sources[0].src;
