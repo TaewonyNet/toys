dc6ff3f75726c124bd6369fb189f4440b714c4f4
TaewonyNet <kingtw@nate.com>
2022-11-22 04:50:13 +0900

    .

--- tutils.js
+++ tutils.js
@@ -1,25 +1,25 @@
 function test_code() {
     tspt = document.createElement('script');
-    tspt.setAttribute('src','//rawgit.com/TaewonyNet/toys/master/tutils.js');
-    tspt.setAttribute('onload','tutilsInit()');
+    tspt.setAttribute('src', '//rawgit.com/TaewonyNet/toys/master/tutils.js');
+    tspt.setAttribute('onload', 'tutilsInit()');
     document.head.appendChild(tspt);
-    function tutilsInit(){
+    function tutilsInit() {
         tutils.visitStyle;
         tutils.InRemoveElements = [
         ];
         tutils.StorageManager.View(document.body);
-        tutils.StorageManager.Add(tutils.Storage.length+'<br>');
+        tutils.StorageManager.Add(tutils.Storage.length + '<br>');
     }
 }
 
 let tutils = {
     // 방문 사이트 강조
-    is_visitStyle : false,
+    is_visitStyle: false,
     get visitStyle() {
         this.is_visitStyle = !this.is_visitStyle;
         if (this.is_visitStyle) {
             this.visitStyleElement = document.createElement('style');
-            this.visitStyleElement.setAttribute('type','text/css');
+            this.visitStyleElement.setAttribute('type', 'text/css');
             this.visitStyleElement.innerHTML = 'a {border-left: solid 20px green;} a:visited {color:white;border-color: red;}'
             document.head.appendChild(this.visitStyleElement);
         } else {
@@ -28,47 +28,48 @@ let tutils = {
         }
     },
     // 보기싫은 게시물 삭제
-    inRemoveRegexText : [],
+    inRemoveRegexText: [],
     set InRemoveElements(value) {
         this.inRemoveRegexText = value;
         var notin = this.inRemoveRegexText.map(function (f) { return new RegExp(f, 'i') });
         tag_a = 'a:not([href^=javascript])';
-        function e_r(d, t) { 
-            if (d == null) return [];
-            return [...d.querySelectorAll(t)].reverse(); 
-        } 
+        function e_r(d, t) {
+            if (d == null) { return [] };
+            return [...d.querySelectorAll(t)].reverse();
+        }
         e_r(document, tag_a).forEach(function (ele) {
             for (var j = 0; j < notin.length; j++) {
-                var tag = ele;
-                for (var i = 0; i < 10; i++) {
-                    //console.log(tag, e_r(tag.parentElement, tag_a));
-                    if (e_r(tag.parentElement, tag_a).length > 2) {
-                        if (notin[j].exec(ele.outerHTML) != null) {
+                if (notin[j].exec(ele.outerHTML) != null) {
+                    //console.log(ele);
+                    var tag = ele;
+                    for (var i = 0; i < 10; i++) {
+                        console.log(tag, e_r(tag.parentElement, tag_a));
+                        if (e_r(tag.parentElement, tag_a).length > 1) {
                             console.log(tag, e_r(tag.parentElement, tag_a));
                             tag.remove();
                             break;
                         }
+                        else {
+                            tag = tag.parentElement;
+                        }
                     }
-                    else {
-                        tag = tag.parentElement;
-                    }
+                    if (tag.parentElement == null) { break; }
                 }
-                if (tag.parentElement == null) { break; }
             }
-        }) 
+        })
     },
     // 로컬스토리지 추가/삭제
-    localStorageName : 'tutils',
+    localStorageName: 'tutils',
     get Storage() {
         var storage = localStorage.getItem(this.localStorageName);
-        if (!storage) { storage = '[]' } 
+        if (!storage) { storage = '[]' }
         return JSON.parse(storage);
     },
     set Storage(value) {
         localStorage.setItem(this.localStorageName, JSON.stringify(value));
     },
-    StorageManager : {
-        Add : function(items) {
+    StorageManager: {
+        Add: function (items) {
             if (items.constructor == Array) {
                 tutils.Storage = tutils.Storage.concat(...items);
             }
@@ -77,7 +78,7 @@ let tutils = {
             }
             this.Reload();
         },
-        Remove : function(items) {
+        Remove: function (items) {
             var rm = [];
             if (items.constructor == Array) {
                 rm = rm.concat(...items);
@@ -95,7 +96,7 @@ let tutils = {
             tutils.Storage = storage;
             this.Reload();
         },
-        RemoveIndex : function(items) {
+        RemoveIndex: function (items) {
             var rm = [];
             if (items.constructor == Array) {
                 rm = rm.concat(...items);
@@ -114,7 +115,7 @@ let tutils = {
             tutils.Storage = storage;
             this.Reload();
         },
-        View : function (element) {
+        View: function (element) {
             if (tutils.listElement != null) {
                 tutils.listElement.remove();
             }
@@ -123,7 +124,7 @@ let tutils = {
             element.insertBefore(tutils.listElement, element.firstChild);
             this.Reload();
         },
-        Reload : function() {
+        Reload: function () {
             if (tutils.listElement == null) {
                 return;
             }
@@ -137,6 +138,6 @@ let tutils = {
             }
         },
     },
-    listElement : null,
+    listElement: null,
 };
 tutils.Storage;
